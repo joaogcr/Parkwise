0b2fcace54ab0285a87efc7e2613e024a4addd36
ni-teixeira <nicoly.teixeira@sptech.school>
2024-10-22 20:39:47 -0300

    mudancas na modelagem, script da vm e api

--- API/main.js
+++ API/main.js
@@ -8,7 +8,7 @@ const SERIAL_BAUD_RATE = 9600;
 const SERVIDOR_PORTA = 3300;
 
 // habilita ou desabilita a inserção de dados no banco de dados
-const HABILITAR_OPERACAO_INSERIR = true;
+const HABILITAR_OPERACAO_INSERIR = false;
 
 // função para comunicação serial
 const serial = async (
@@ -19,8 +19,8 @@ const serial = async (
     let poolBancoDados = mysql.createPool(
         {
             host: 'localhost',
-            user: 'parkwise',
-            password: 'Projeto123!',
+            user: 'ParkWise',
+            password: 'Urubu@100',
             database: 'parkwisePI',
             port: 3307
         }
@@ -60,7 +60,7 @@ const serial = async (
 
             // este insert irá inserir os dados na tabela "medida"
             await poolBancoDados.execute(
-                'INSERT INTO insercoesVM (sensor_digital) VALUES (?)',
+                'INSERT INTO fluxo (statusVaga) VALUES (?)',
                 [sensorDigital]
             );
             console.log("valores inseridos no banco: " + sensorDigital);
