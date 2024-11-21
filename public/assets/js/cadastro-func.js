6a17638d4f4672b2c5c09a2da0929e5ec08adffe
Ariel-Rocha <ariel.rocha@sptech.school>
2024-11-20 23:45:25 -0300

    pagina cadastro-func.js, inicio de validações - Ariel

---
+++ public/assets/js/cadastro-func.js
@@ -0,0 +1,9 @@
+function cadastrarFunc() {
+    // Capturando os valores diretamente das variáveis input
+    var nome = ipt_nome.value;
+    var email = ipt_email.value;
+    var senha = ipt_senha.value;
+    var funcao = ipt_funcao.value;
+    var estacionamento = ipt_estacionamento.value;
+} 
+var mensagem = '';
