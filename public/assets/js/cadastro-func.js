6eea1e3f133632a4dd51ee0a8167b90c69a4ff41
Ariel-Rocha <ariel.rocha@sptech.school>
2024-11-21 21:30:54 -0300

    validação-campo nome, obrigatório- cadastrofunc

--- public/assets/js/cadastro-func.js
+++ public/assets/js/cadastro-func.js
@@ -1,9 +1,15 @@
 function cadastrarFunc() {
-    // Capturando os valores diretamente das variáveis input
+
     var nome = ipt_nome.value;
     var email = ipt_email.value;
     var senha = ipt_senha.value;
     var funcao = ipt_funcao.value;
     var estacionamento = ipt_estacionamento.value;
-} 
+}
+
 var mensagem = '';
+
+
+if (nome === '') {
+    mensagem += '⚠️ "Nome" é um campo obrigatório.<br>';
+}
