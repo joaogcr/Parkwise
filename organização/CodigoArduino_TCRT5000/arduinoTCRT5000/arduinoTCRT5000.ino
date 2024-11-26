ac447c36028062b3c436ae5dc3d46768e2b1ea69
joaogcr <joao.nribeiro@sptech.school>
2024-11-26 20:41:02 -0300

    Código C do Arduino

--- organização/CodigoArduino_TCRT5000/arduinoTCRT5000/arduinoTCRT5000.ino
+++ organização/CodigoArduino_TCRT5000/arduinoTCRT5000/arduinoTCRT5000.ino
@@ -1,31 +1,24 @@
-const int PINO_SENSOR_TCRT5000 = 7; // número da entrada digital
-  int ativado = 0; // variável global para a presença do carro
-  int desativado = 0; // variável global para vaga vazia
-  int const separacao = 0;
+const int PINO_SENSOR_TCRT5000 = 7;
+int estadoAnterior = -1;
+int estadoAtual;
 
-void setup() { // inicia a configuração
-  pinMode(PINO_SENSOR_TCRT5000, INPUT); // configura o pino como entrada (INPUT)
-  Serial.begin(9600); // velocidade de leitura de bits por segundo
+void setup() {
+  pinMode(PINO_SENSOR_TCRT5000, INPUT);
+  Serial.begin(9600);
 }
 
-void loop() { // inica a contagem das variáveis
-  int estadoSensor = digitalRead(PINO_SENSOR_TCRT5000); // leitura do valor que o sensor está devolvendo
-  String p1=";";
+void loop() {
+  estadoAtual = digitalRead(PINO_SENSOR_TCRT5000);
 
-  if (estadoSensor == LOW) {
-    desativado = 0; // zera o valor de desativado quando sair um carro
-    Serial.print(ativado);
-    Serial.print(p1);
-    Serial.println(desativado);
-    ativado++; // incrementa o valor quando tem carro
-  } else {
-    ativado = 0; // zera o valor do ativado
-    delay(1000);
-    Serial.print(ativado);
-    Serial.print(p1);
-    Serial.println(desativado);
-    desativado--; // incrementa o valor quando não tem carro
+  if (estadoAtual != estadoAnterior) {
+    estadoAnterior = estadoAtual;
+
+    if (estadoAtual == LOW) {
+      Serial.println("1");
+    } else {
+      Serial.println("0");
+    }
   }
 
-  delay(1000); // cria um atraso de 1 segundo 1
+  delay(1000);
 }
